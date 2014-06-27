a7b6986435c73dbfc30c9d2c4f2dd128bd6ef857
Olivier <sabinus52@gmail.com>
2014-06-27 17:43:43 +0200

    Prise en compte d'un script personnalisé pour l'installation d'un projet

--- modules/project-install/project-install.sh
+++ modules/project-install/project-install.sh
@@ -41,66 +41,83 @@ read REPONSE
 [ "$REPONSE" != "o" ] && return 0
 
 
-
 ###
-# Installation de paquets additionnels
+# Si script personnalisé alors celui-ci est préféré
 ##
-echo
-echo -e "${CVIOLET} Installation de paquets additionnels ${CVOID}"
-echo -e "${CBLANC}-------------------------------------------------------------------------------${CVOID}"
-if [[ ! -z ${OLIX_CONF_PROJECT_INSTALL_APT} ]]; then
-	logger_debug "apt-get install ${OLIX_CONF_PROJECT_INSTALL_APT}"
-	sudo apt-get --yes install ${OLIX_CONF_PROJECT_INSTALL_APT}
-	[[ $? -ne 0 ]] && logger_error
-fi
+__FILE_SCRIPT=$(project_getFileConf "${OLIX_PROJECT_PATH_CONFIG}/install" ".sh")
+if [[ ! -z ${__FILE_SCRIPT} ]]; then
 
-
-
-###
-# Installation via la synchronisation du dossier 
-##
-echo
-echo -e "${CVIOLET} Copie des fichiers dans ${CCYAN}${OLIX_CONF_PROJECT_PATH}${CVOID}"
-echo -e "${CBLANC}-------------------------------------------------------------------------------${CVOID}"
-
-stdin_readConnexionServerSSH ${OLIX_PROJECT_CODE}
-
-filesystem_synchronize "${OLIX_STDIN_SERVER_PORT}" \
-                       "${OLIX_STDIN_SERVER_USER}@${OLIX_STDIN_SERVER_HOST}:${OLIX_STDIN_SERVER_PATH}" \
-                       "${OLIX_CONF_PROJECT_PATH}" \
-                       "${OLIX_CONF_PROJECT_INSTALL_FILE_EXCLUDE}"
-[[ $? -ne 0 ]] && logger_error
-
-
-
-###
-# Installation des bases de données
-##
-for I in ${OLIX_CONF_PROJECT_MYSQL_BASE}; do
+	###
+	# Execution du script personnalisé
+	##
 	echo
-	echo -e "${CVIOLET} Installation de la base ${CCYAN}${I}${CVOID}"
+	echo -e "${CVIOLET} Execution du script personnalisé ${CCYAN}${__FILE_SCRIPT}${CVOID}"
+	echo -e "${CBLANC}-------------------------------------------------------------------------------${CVOID}"
+	source ${__FILE_SCRIPT}
+
+
+else
+
+	###
+	# Installation de paquets additionnels
+	##
+	echo
+	echo -e "${CVIOLET} Installation de paquets additionnels ${CVOID}"
+	echo -e "${CBLANC}-------------------------------------------------------------------------------${CVOID}"
+	if [[ ! -z ${OLIX_CONF_PROJECT_INSTALL_APT} ]]; then
+		logger_debug "apt-get install ${OLIX_CONF_PROJECT_INSTALL_APT}"
+		sudo apt-get --yes install ${OLIX_CONF_PROJECT_INSTALL_APT}
+		[[ $? -ne 0 ]] && logger_error
+	fi
+
+
+
+	###
+	# Installation via la synchronisation du dossier 
+	##
+	echo
+	echo -e "${CVIOLET} Copie des fichiers dans ${CCYAN}${OLIX_CONF_PROJECT_PATH}${CVOID}"
 	echo -e "${CBLANC}-------------------------------------------------------------------------------${CVOID}"
 
-    mysql_dropDatabaseLocal "${I}"
-    [[ $? -ne 0 ]] && logger_error "Impossible de supprimer la base $I"
+	stdin_readConnexionServerSSH ${OLIX_PROJECT_CODE}
 
-    mysql_createDatabaseLocal "${I}"
-    [[ $? -ne 0 ]] && logger_error "Impossible de créer la base $I"
+	filesystem_synchronize "${OLIX_STDIN_SERVER_PORT}" \
+	                       "${OLIX_STDIN_SERVER_USER}@${OLIX_STDIN_SERVER_HOST}:${OLIX_STDIN_SERVER_PATH}" \
+	                       "${OLIX_CONF_PROJECT_PATH}" \
+	                       "${OLIX_CONF_PROJECT_INSTALL_FILE_EXCLUDE}"
+	[[ $? -ne 0 ]] && logger_error
 
-    mysql_createRoleLocal "${OLIX_CONF_PROJECT_MYSQL_USER}" "${OLIX_CONF_PROJECT_MYSQL_PASS}" "${I}"
-    [[ $? -ne 0 ]] && logger_error "Impossible de créer le role de ${OLIX_CONF_PROJECT_MYSQL_USER} sur $I"
-    echo -e "Création de la base : ${CVERT}OK ...${CVOID}"
 
-    [[ "|${OLIX_CONF_PROJECT_INSTALL_BASE_EXCLUDE// /|}|" =~ "|${I}|" ]] && continue
 
-    OLIX_STDIN_SERVER_BASE="${I}"
-	stdin_readConnexionServerMySQL "${OLIX_PROJECT_CODE}.${I}"
+	###
+	# Installation des bases de données
+	##
+	for I in ${OLIX_CONF_PROJECT_MYSQL_BASE}; do
+		echo
+		echo -e "${CVIOLET} Installation de la base ${CCYAN}${I}${CVOID}"
+		echo -e "${CBLANC}-------------------------------------------------------------------------------${CVOID}"
 
-    mysql_synchronizeDatabase "${OLIX_STDIN_SERVER_HOST}" "${OLIX_STDIN_SERVER_PORT}" \
-    						  "${OLIX_STDIN_SERVER_USER}" "${OLIX_STDIN_SERVER_BASE}" "${I}"
-    [[ $? -ne 0 ]] && logger_error "Impossible de synchroniser la base $I"
-done
+	    mysql_dropDatabaseLocal "${I}"
+	    [[ $? -ne 0 ]] && logger_error "Impossible de supprimer la base $I"
 
+	    mysql_createDatabaseLocal "${I}"
+	    [[ $? -ne 0 ]] && logger_error "Impossible de créer la base $I"
+
+	    mysql_createRoleLocal "${OLIX_CONF_PROJECT_MYSQL_USER}" "${OLIX_CONF_PROJECT_MYSQL_PASS}" "${I}"
+	    [[ $? -ne 0 ]] && logger_error "Impossible de créer le role de ${OLIX_CONF_PROJECT_MYSQL_USER} sur $I"
+	    echo -e "Création de la base : ${CVERT}OK ...${CVOID}"
+
+	    [[ "|${OLIX_CONF_PROJECT_INSTALL_BASE_EXCLUDE// /|}|" =~ "|${I}|" ]] && continue
+
+	    OLIX_STDIN_SERVER_BASE="${I}"
+		stdin_readConnexionServerMySQL "${OLIX_PROJECT_CODE}.${I}"
+
+	    mysql_synchronizeDatabase "${OLIX_STDIN_SERVER_HOST}" "${OLIX_STDIN_SERVER_PORT}" \
+	    						  "${OLIX_STDIN_SERVER_USER}" "${OLIX_STDIN_SERVER_BASE}" "${I}"
+	    [[ $? -ne 0 ]] && logger_error "Impossible de synchroniser la base $I"
+	done
+
+fi
 
 
 ###
